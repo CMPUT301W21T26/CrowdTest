b0fd7b2e567555b9faabbc1c20d21b330b89fe36
ndiocson <ndiocson@outlook.com>
2021-04-06 19:19:50 -0600

    removed unecessary imports

--- app/src/main/java/com/example/crowdtest/ui/BinomialActivity.java
+++ app/src/main/java/com/example/crowdtest/ui/BinomialActivity.java
@@ -1,37 +1,16 @@
 package com.example.crowdtest.ui;
 
-import android.Manifest;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.location.Location;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.annotation.RequiresApi;
-import androidx.core.app.ActivityCompat;
 
-import com.example.crowdtest.GetTrials;
 import com.example.crowdtest.R;
 import com.example.crowdtest.experiments.Binomial;
-import com.example.crowdtest.experiments.BinomialTrial;
-import com.example.crowdtest.experiments.Count;
-import com.example.crowdtest.experiments.CountTrial;
-import com.example.crowdtest.experiments.Experiment;
-import com.example.crowdtest.experiments.Measurement;
-import com.example.crowdtest.experiments.MeasurementTrial;
-import com.example.crowdtest.experiments.NonNegative;
-import com.example.crowdtest.experiments.NonNegativeTrial;
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
 
 /**
  * Binomial experiment activity class that has two buttons for submitting success and failure
